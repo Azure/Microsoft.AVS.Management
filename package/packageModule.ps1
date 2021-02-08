18449c1fd8b8f693eaedfc3980f8bb2f07fd241b
Microsoft Open Source <microsoftopensource@users.noreply.github.com>
2021-02-08 10:26:08 -0800

    Initial Repo for AVS Cmdlets
    Initial Repo
    
    Initial SECURITY.md commit
    
    Initial README.md commit
    
    Initial SUPPORT.md commit
    
    moving to new repo
    
    got the module bit working, must run Import-Module on that psd1 file
    
    updated add LDAP to use the new one with certs
    
    working certs with file pass in
    
    changed from file passing to getting the certificate
    
    updated parameter help messages and the LDAP script
    
    Updated the PSD1 file with more information and the proper SsoAdmin dependency
    
    Updated psd1 file to have the proper dependency
    
    Updated psd1 file to have the proper dependency
    
    Improvements to the LDAP script
    
    Working version of LDAP
    
    Improvments and bug fix
    
    Improvements to scripts
    
    changed from switches to be a string value
    
    Logging improvements and bug fixes
    
    Updated PSD
    
    Wrapped errors in try catch
    
    Removed try catch as they were hiding information
    
    Changing certificate path to be a full path instead of an alias
    
    Fixed Plus Sign Errors and validated that LDAPS works
    
    1st commit.
    
    Set up CI with Azure Pipelines
    
    [skip ci]
    
    Update README.md
    
    Cleaning up the repo. (#15)
    
    * Improvements to functions to make them more idiomatic
    
    * Improved help and examples
    
    * Removed unecessary files
    
    * Adding scripting guidelines
    
    Co-authored-by: David Becher <dabecher@microsoft.com>
    
    Improvements to AVS Script - Bug Fixes and Logging Improvements (#16)
    
    * Bug fixes and better logging
    
    Authored-by: David Becher <dabecher@microsoft.com>
    
    Removing some optional parameters to have more clear cmdlets (#18)
    
    * Removed some optional params, instead making multiple cmdlets with cleaner instruction
    
    * Updated examples
    
    * Adding tags/projecturi/requiredmodule
    
    Co-authored-by: David Becher <dabecher@microsoft.com>
    
    Repo Initialization (#20)
    
    * moving to new repo
    
    * Initial CODE_OF_CONDUCT.md commit
    
    * Initial LICENSE commit
    
    * Initial SECURITY.md commit
    
    * Initial README.md commit
    
    * Initial SUPPORT.md commit
    
    * got the module bit working, must run Import-Module on that psd1 file
    
    * updated add LDAP to use the new one with certs
    
    * 1st commit.
    
    * Set up CI with Azure Pipelines
    
    [skip ci]
    
    * working certs with file pass in
    
    * changed from file passing to getting the certificate
    
    * updated parameter help messages and the LDAP script
    
    * updated test function
    
    * Updated the PSD1 file with more information and the proper SsoAdmin dependency
    
    Updated psd1 file to have the proper dependency
    
    Updated psd1 file to have the proper dependency
    
    * Improvements to the LDAP script
    
    * Working version of LDAP
    
    * Edit yaml for ADO build pipelines.
    
    * Improvments and bug fix
    
    * Improvements to scripts
    
    * changed from switches to be a string value
    
    * Logging improvements and bug fixes
    
    * Updated PSD
    
    * Wrapped errors in try catch
    
    * Removed try catch as they were hiding information
    
    * Changing certificate path to be a full path instead of an alias
    
    * Fixed Plus Sign Errors and validated that LDAPS works
    
    * Trimmed yaml files.
    
    * Silently continue on Test-ModuleManifest error.
    
    * Update README.md
    
    * Cleaning up the repo. (#15)
    
    * Improvements to functions to make them more idiomatic
    
    * Improved help and examples
    
    * Removed unecessary files
    
    * Adding scripting guidelines
    
    Co-authored-by: David Becher <dabecher@microsoft.com>
    
    * Improvements to AVS Script - Bug Fixes and Logging Improvements (#16)
    
    * Bug fixes and better logging
    
    Authored-by: David Becher <dabecher@microsoft.com>
    
    * Removed some optional params, instead making multiple cmdlets with cleaner instruction
    
    * Updated examples
    
    * Adding tags/projecturi/requiredmodule
    
    Co-authored-by: David Becher <dabecher@microsoft.com>
    Co-authored-by: Microsoft Open Source <microsoftopensource@users.noreply.github.com>
    Co-authored-by: Michael Barta <michaelbarta@microsoft.com>
    Co-authored-by: greymiba <56175918+greymiba@users.noreply.github.com>

---
+++ package/packageModule.ps1
@@ -0,0 +1,61 @@
+#!/usr/bin/pwsh
+param (
+    [Parameter(Mandatory=$true)][string]$srcFolder,
+    [Parameter(Mandatory=$true)][string]$newModuleFolder,
+    [Parameter(Mandatory=$true)][string]$buildType
+)
+$feedParameters = @{}
+# if ($buildType -eq 'official') {
+#     $feedParameters = @{
+#         Name = 'AVS-Automation-AdminTools'
+#         SourceLocation = "https://pkgs.dev.azure.com/mseng/AzureDevOps/_packaging/AVS-Automation-AdminTools/nuget/v3/index.json"
+#         PublishLocation = "https://pkgs.dev.azure.com/mseng/AzureDevOps/_packaging/AVS-Automation-AdminTools/nuget/v3/index.json"
+#         InstallationPolicy = 'Trusted'
+#     }
+# }else
+if ($buildType -eq 'unofficial' -or $buildType -eq 'official') {
+    $feedParameters = @{
+        Name = "Unofficial-AVS-Automation-AdminTools"
+        SourceLocation = "https://pkgs.dev.azure.com/avs-oss/Public/_packaging/Unofficial-AVS-Automation-AdminTools/nuget/v3/index.json"
+        PublishLocation = "https://pkgs.dev.azure.com/avs-oss/Public/_packaging/Unofficial-AVS-Automation-AdminTools/nuget/v3/index.json"
+        InstallationPolicy = 'Trusted'
+    }
+}else {
+    Write-Error -Message "----Error: Unsupported buildType: $buildType----" -ErrorAction Stop
+}
+Write-Output "Available repositories:"
+Get-PSRepository
+
+Write-Output "----Registering PSRepository ----"
+Register-PSRepository @feedParameters
+if (!$?) {
+    Write-Error -Message "----ERROR: Unable to register repository----" -ErrorAction Stop
+}else {
+    
+    Write-Output "----SUCCEEDED: repository registered ----"
+}
+Write-Output "Available repositories:"
+Get-PSRepository
+
+$repoRoot = "$env:SYSTEM_DEFAULTWORKINGDIRECTORY"
+$aboluteSrcFolderPath = (Join-Path -Path "$repoRoot" -ChildPath "$srcFolder")
+$aboluteNewFolderPath = (Join-Path -Path "$repoRoot" -ChildPath "$newModuleFolder")
+if (!(Test-Path "$aboluteNewFolderPath")) {
+    Write-Output "Copying directory contents: $aboluteSrcFolderPath --> $aboluteNewFolderPath"
+    New-Item -Path "$aboluteNewFolderPath" -ItemType Directory
+    Copy-Item -Path "$aboluteSrcFolderPath\*" -Destination "$aboluteNewFolderPath"
+}else{
+    Write-Output "----Path for new module directory already exists ----"
+}
+Write-Output "Contents of new directory: $aboluteNewFolderPath"
+Get-ChildItem "$aboluteNewFolderPath"
+
+Write-Host "----AVS-Automation-AdminTools: publishing $buildType build package ----"
+Publish-Module -Path "$aboluteNewFolderPath" -Repository ($feedParameters).Name -NuGetApiKey "valueNotUsed"
+if (!$?) {
+    Write-Error -Message "----ERROR: Unable to publish module----" -ErrorAction Stop
+}else {
+    Write-Output "SUCCEEDED: module published"
+}
+
+Write-Host "----AVS-Automation-AdminTools: Azure.AVSPowerCLI nuget package deposited----"
