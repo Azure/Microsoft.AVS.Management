a5daeb9397cf39f107ae3d7d7a7335c7898c06dd
Michael Barta <michaelbarta@microsoft.com>
2021-04-23 22:12:45 -0700

    construct nuget package (2)

--- build/build.ps1
+++ build/build.ps1
@@ -1,20 +1,17 @@
 #!/usr/bin/pwsh
 $repoRoot = "$env:SYSTEM_DEFAULTWORKINGDIRECTORY"
-#$signedModulesLoc = "signedPSModules"
-$modulesDirectory = "powercli"
+$artifactDirectoryRoot = "$env:BUILD_ARTIFACTSTAGINGDIRECTORY"
+$signedModulesLoc = "signedPSModules"
+# $modulesDirectory = "powercli"
 $outputFolderName = "adminToolBuildOutput"
 
-Set-Location "$repoRoot\$modulesDirectory"
-#Create folder for build output
-# $commit = (Get-Date -Format "yyyyMMddTHHmmssffffZ")
-# if (($null -ne $env:CDP_COMMIT_ID) -and ($env:CDP_COMMIT_ID -ne "")) {
-#     $commit = ${env:CDP_COMMIT_ID}
-# } elseif (($null -ne $env:BUILD_SOURCEVERSION) -and ($env:BUILD_SOURCEVERSION -ne "*")) {
-#     $commit = ${env:BUILD_SOURCEVERSION}.substring(0,8)
-# }
 New-Item -Path $repoRoot -Name "$outputFolderName" -ItemType "directory"
+Copy-Item -Path "$artifactDirectoryRoot\$signedModulesLoc\*" -Destination "$repoRoot\$outputFolderName" -Recurse
+Set-Location "$repoRoot\$outputFolderName"
 
 Write-Host "----AVS-Automation-AdminTools: making nuget package ----"
 nuget spec Azure.AVSPowerCLI
-nuget pack Azure.AVSPowerCLI.nuspec -NonInteractive -OutputDirectory "$repoRoot\$outputFolderName"
+nuget pack Azure.AVSPowerCLI.nuspec -NonInteractive
 Write-Host "----AVS-Automation-AdminTools: Azure.AVSPowerCLI nuget package deposited in $repoRoot\$outputFolderName----"
+
+Set-Location "$repoRoot"
