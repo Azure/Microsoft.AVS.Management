30cda9afc0f4e6602358c811c660b3e96780ba0e
Michael Barta <michaelbarta@microsoft.com>
2021-04-29 00:13:27 -0700

    add version for unofficial build. (11)

--- build/build.ps1
+++ build/build.ps1
@@ -17,7 +17,7 @@ Set-Location (Join-Path "$artifactDirRoot" "$newModuleFolder")
 Get-Content "Azure.AVSPowerCLI.psd1"
 Write-Host "----AVS-Automation-AdminTools: making nuget package ----"
 nuget spec Azure.AVSPowerCLI
-nuget pack Azure.AVSPowerCLI.nuspec -NonInteractive
+nuget pack Azure.AVSPowerCLI.nuspec -NonInteractive -Version "$env:BUILD_BUILDNUMBER"
 Write-Host "----AVS-Automation-AdminTools: Azure.AVSPowerCLI nuget package deposited----"
 
 Get-Content "Azure.AVSPowerCLI.psd1"
