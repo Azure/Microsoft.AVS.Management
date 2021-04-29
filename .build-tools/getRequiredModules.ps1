e6e8bc672664d10dbc0c938ce17d66f026ed077c
Michael Barta <michaelbarta@microsoft.com>
2021-04-28 20:05:28 -0700

    add version for unofficial build.

---
+++ .build-tools/getRequiredModules.ps1
@@ -0,0 +1,16 @@
+#!/usr/bin/pwsh
+
+$requiredModules = @(
+@{"ModuleName"="VMware.VimAutomation.Common";"ModuleVersion"="12.0.0.15939652"}
+)
+Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
+foreach ($module in $requiredModules) {
+    $targetModule = $module.ModuleName
+    $targetVersion = $module.ModuleVersion
+    Write-Host "Installing $targetModule-$targetVersion ...."
+    Install-Module $targetModule -RequiredVersion $targetVersion
+    Write-Host "----COMPLETED installation of $targetModule-$targetVersion----"
+}
+
+
+Write-Host "----SUCCESS: installed all required modules----"
---
+++ .build-tools/updateModuleVersion.ps1
@@ -0,0 +1,12 @@
+#!/usr/bin/pwsh
+#Install all RequiredModules in the module manifests because the command Update-ModuleManifest 
+# requires these modules to be on the host to update properly.
+$repoRoot = "$env:SYSTEM_DEFAULTWORKINGDIRECTORY"
+$manifestFolder = (Join-Path "powercli" "Azure.AVSPowerCLI")
+$manifestFile = "Azure.AVSPowerCLI.psd1"
+$pathToManifest = (Join-Path "$repoRoot" "$manifestFolder" "$manifestFile")
+
+Write-Output "---- Updating the module version to $(env:BUILD_BUILDNUMBER)----"
+$targetModuleParams = @{ModuleVersion = "$(env:BUILD_BUILDNUMBER)"; Path = "$pathToManifest"}
+Update-ModuleManifest @targetModuleParams
+Write-Output "---- SUCCESS: updated the module version to $(env:BUILD_BUILDNUMBER)----"
