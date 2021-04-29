30752f69758f72aa05d73c9958f2541da58f7eeb
Michael Barta <michaelbarta@microsoft.com>
2021-04-28 20:14:05 -0700

    add version for unofficial build. (2)

--- .build-tools/updateModuleVersion.ps1
+++ .build-tools/updateModuleVersion.ps1
@@ -6,7 +6,7 @@ $manifestFolder = (Join-Path "powercli" "Azure.AVSPowerCLI")
 $manifestFile = "Azure.AVSPowerCLI.psd1"
 $pathToManifest = (Join-Path "$repoRoot" "$manifestFolder" "$manifestFile")
 
-Write-Output "---- Updating the module version to $(env:BUILD_BUILDNUMBER)----"
-$targetModuleParams = @{ModuleVersion = "$(env:BUILD_BUILDNUMBER)"; Path = "$pathToManifest"}
+Write-Output "---- Updating the module version to $env:BUILD_BUILDNUMBER----"
+$targetModuleParams = @{ModuleVersion = "$env:BUILD_BUILDNUMBER"; Path = "$pathToManifest"}
 Update-ModuleManifest @targetModuleParams
-Write-Output "---- SUCCESS: updated the module version to $(env:BUILD_BUILDNUMBER)----"
+Write-Output "---- SUCCESS: updated the module version to $env:BUILD_BUILDNUMBER----"
