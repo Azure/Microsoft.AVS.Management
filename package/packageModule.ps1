acd185c1f0cd6150a1c6cc463771208b0473d11f
Michael Barta <michaelbarta@microsoft.com>
2021-05-07 18:55:12 -0700

    Modified build scripts.(2)

--- package/packageModule.ps1
+++ package/packageModule.ps1
@@ -4,26 +4,29 @@ param (
     [Parameter(Mandatory=$true)][string]$newModuleFolder
 )
 $repoRoot = "$env:SYSTEM_DEFAULTWORKINGDIRECTORY"
-#$artifactDirRoot = "$env:BUILD_ARTIFACTSTAGINGDIRECTORY"
 $aboluteSrcFolderPath = (Join-Path -Path "$repoRoot" -ChildPath "$srcFolder")
 $aboluteNewFolderPath = (Join-Path -Path "$repoRoot" -ChildPath "$newModuleFolder")
 if (!(Test-Path "$aboluteNewFolderPath")) {
+    Write-Output "Copying directory contents: $aboluteSrcFolderPath --> $aboluteNewFolderPath"
     New-Item -Path "$aboluteNewFolderPath" -ItemType Directory
     Copy-Item -Path "$aboluteSrcFolderPath" -Destination "$aboluteNewFolderPath"
+}else{
+    Write-Output "----Path for new module directory already exists ----"
 }
 $manifestFiles = (Get-ChildItem "$aboluteNewFolderPath\*" -Include "*.psd1")
+Write-Output "Contents of new directory: $aboluteNewFolderPath"
+Get-ChildItem "$aboluteNewFolderPath"
 $manifestFile = ""
-if ($manifestFiles.Count -gt 0) {
+if (($manifestFiles).Count -gt 0) {
     $manifestFile = $manifestFiles[0].FullName
 }else {
-    Write-Error -Message "----Packagin Failed: No manifest file found----" -ErrorAction Stop
+    Write-Error -Message "----Packaging Failed: No manifest file found----" -ErrorAction Stop
 }
 Set-Location "$aboluteNewFolderPath"
 Get-Content "$manifestFile"
 Write-Host "----AVS-Automation-AdminTools: making nuget package ----"
 nuget spec "$manifestFile"
-nuget pack "$manifestFile.nuspec" -NonInteractive -Version "$env:BUILD_BUILDNUMBER"
+nuget pack "$($manifestFile).nuspec" -NonInteractive -Version "$env:BUILD_BUILDNUMBER"
 Write-Host "----AVS-Automation-AdminTools: Azure.AVSPowerCLI nuget package deposited----"
 
 Get-Content "$manifestFile"
-#Set-Location "$repoRoot"
