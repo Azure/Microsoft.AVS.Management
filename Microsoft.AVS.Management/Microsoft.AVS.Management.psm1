2dfd4fa40f6715ac799d579a01d7bca58098151c
David Becher <dabecher@microsoft.com>
2021-06-02 02:51:44 -0400

    Improved metadata

--- Microsoft.AVS.Management/Microsoft.AVS.Management.psm1
+++ Microsoft.AVS.Management/Microsoft.AVS.Management.psm1
@@ -1,7 +1,6 @@
 #Requires -Modules PowerShellGet
 #Requires -Version 5.0
 
-
 <#
 AVSAttribute applied to a commandlet function indicates:
 - wether the SDDC should be marked as Building while the function executes.
@@ -24,10 +23,9 @@ class AVSAttribute : Attribute {
 ========================================================================================================
 #>
 
-# Exported Functions
 <#
     .Synopsis
-     Allow customers to add an external identity source (Active Directory over LDAP) for use with single sign on to vCenter. Prefaced by Connect-SsoAdminServer
+     (NOT RECOMMENDED -> Use New-AvsLDAPSIdentitySource) Allow customers to add an external identity source (Active Directory over LDAP) for use with single sign on to vCenter. Prefaced by Connect-SsoAdminServer
 
     .Parameter Name
      The user-friendly name the external AD will be given in vCenter
@@ -38,9 +36,24 @@ class AVSAttribute : Attribute {
     .Parameter DomainAlias 
      Domain alias of the external active directory, e.g. myactivedirectory
 
+    .Parameter PrimaryUrl
+     Url of the primary ldap server to attempt to connect to, e.g. ldap://myadserver.local:389
+    
+    .Parameter SecondaryUrl 
+     Url of the fallback ldap server to attempt to connect to, e.g. ldap://myadserver.local:389
+
+    .Parameter BaseDNUsers 
+     Base Distinguished Name for users, e.g. "dc=myadserver,dc=local"
+
+    .Parameter BaseDNGroups
+     Base Distinguished Name for groups, e.g. "dc=myadserver,dc=local"
+
+    .Parameter Credential 
+     Credential to login to the LDAP server (NOT cloudAdmin) in the form of a username/password credential
+
     .Example 
     # Add the domain server named "myserver.local" to vCenter
-    Add-AvsLDAPIdentitySource -Name 'myserver' -DomainName 'myserver.local' -DomainAlias 'myserver' -PrimaryUrl 'ldap://10.40.0.5:389' -BaseDNUsers 'dc=myserver, dc=local' -BaseDNGroups 'dc=myserver, dc=local' -Username 'myserver@myserver.local' -Password 'PlaceholderPassword'
+    Add-AvsLDAPIdentitySource -Name 'myserver' -DomainName 'myserver.local' -DomainAlias 'myserver' -PrimaryUrl 'ldap://10.40.0.5:389' -BaseDNUsers 'dc=myserver, dc=local' -BaseDNGroups 'dc=myserver, dc=local'
 #>
 function New-AvsLDAPIdentitySource {
     [CmdletBinding(PositionalBinding = $false)]
@@ -133,6 +146,21 @@ function New-AvsLDAPIdentitySource {
 
     .Parameter DomainAlias 
      Domain alias of the external active directory, e.g. myactivedirectory
+
+    .Parameter PrimaryUrl
+     Url of the primary ldap server to attempt to connect to, e.g. ldap://myadserver.local:389
+    
+    .Parameter SecondaryUrl 
+     Url of the fallback ldap server to attempt to connect to, e.g. ldap://myadserver.local:389
+
+    .Parameter BaseDNUsers 
+     Base Distinguished Name for users, e.g. "dc=myadserver,dc=local"
+
+    .Parameter BaseDNGroups
+     Base Distinguished Name for groups, e.g. "dc=myadserver,dc=local"
+
+    .Parameter Credential 
+     Credential to login to the LDAP server (NOT cloudAdmin) in the form of a username/password credential
 
     .Parameter CertificatesSAS
      An array of Shared Access Signature strings to the certificates required to connect to the external active directory, if using LDAPS
@@ -260,6 +288,21 @@ function New-AvsLDAPSIdentitySource {
     .Synopsis
      Creates a Drs Cluster Host Group, a Drs Cluster VM Group, and a Drs Cluster Virtual Machine to Host Rule between the two
 
+    .Parameter DrsRuleName
+     User-Friendly Name of the Drs VMHost Rule to be created. 
+    
+    .Parameter DrsGroupName
+     User-Friendly prefix of the two Drs Cluster Groups to be created. For example, -DrsGroupName "mygroup" will create a VM group called "mygroup" and a VMHost group called "mygrouphost" 
+
+    .Parameter Cluster
+     Name of the cluster to create the two groups and rule on. The VMs and VMHosts' must be on this cluster
+
+    .Parameter VMList
+     A comma delimited list with the names of the VMs to put in the Drs group 
+
+    .Parameter VMHostList
+     A comma delimited list with the names of the VMHosts' to put in the Drs group 
+
     .Example 
     # Create a should run rule named MyDrsRule on Cluster-1 Hosts using the listed VM's and VMHosts
     New-AvsDrsElevationRule -DrsGroupName "MyDrsGroup" -DrsRuleName "MyDrsRule" -Cluster "Cluster-1" -VMList "vm1", "vm2" -VMHostList "esx01", "esx02"
@@ -323,7 +366,16 @@ function New-AvsDrsElevationRule {
 
 <#
     .Synopsis
-     Edits a VM Drs Cluster Group
+     Edits a VM Drs Cluster Group by adding or removing VMs to or from the group
+
+    .Parameter DrsGroupName
+     Existing VM Drs Cluster Group to edit
+
+    .Parameter VMList
+     A comma delimited list with the names of the VMs to add or remove to/from the Drs group 
+
+    .Parameter Action
+     The action to perform, either "add" or "remove" the VMHosts specified to/from the DrsGroup
 
     .Example 
     # Edit an existing drs group named "MyDrsGroup" on Cluster-1 Hosts adding the listed VM's '
@@ -381,11 +433,20 @@ function Set-AvsDrsVMClusterGroup {
 
 <#
     .Synopsis
-     Edits a VMHost Drs Cluster Group
+     Edits a VMHost Drs Cluster Group by adding or removing VMHosts to or from the group
+
+    .Parameter DrsGroupName
+     Existing VMHost Drs Cluster Group to edit
+
+    .Parameter VMHostList
+     A comma delimited list with the names of the VMHosts' to add or remove to/from the Drs group 
+
+    .Parameter Action
+     The action to perform, either "add" or "remove" the VMHosts' specified to/from the DrsGroup
 
     .Example 
     # Edit an existing drs group named "MyDrsGroup" on Cluster-1 Hosts removing the listed VM Hosts '
-    Set-AvsDrsClusterGroup -DrsGroupName "MyDrsGroup" -Cluster "Cluster-1" -VMHostList "vmHost1", "vmHost2"  -Action "remove"
+    Set-AvsDrsVMHostClusterGroup -DrsGroupName "MyDrsGroup" -Cluster "Cluster-1" -VMHostList "vmHost1", "vmHost2"  -Action "remove"
 #>
 function Set-AvsDrsVMHostClusterGroup {
     [CmdletBinding(PositionalBinding = $false)]
@@ -442,6 +503,15 @@ function Set-AvsDrsVMHostClusterGroup {
     .Synopsis
      Edits a Drs Elevation Rule. Allowed operations are enable/disable and renaming.
 
+    .Parameter DrsRuleName
+     Name of an exisitng Drs Rule to edit
+
+    .Parameter Enabled
+     Set to $true to enable the Drs Rule, $false to disable it
+
+    .Parameter NewName
+     If specified, the name to change the DrsRule to
+
     .Example 
     # Enable and change the name of a drs rule named "myDrsRule"
     Set-AvsDrsElevationRule -DrsRuleName "myDrsRule"  -Enabled $true -NewName "mynewDrsRule"
@@ -490,10 +560,16 @@ function Set-AvsDrsElevationRule {
   
 <#
     .Synopsis
-     Edit the storage policy on the VM to a predefined storage policy
+     Sets the storage policy on the VM to a predefined storage policy
+
+    .Parameter StoragePolicyName
+     Name of a storage policy to set on the specified VM. Options can be seen in vCenter.
+
+    .Parameter VMName
+     Name of the VM to set the storage policy on.
 
     .Example 
-    # Create a should run rule named MyDrsRule on Cluster-1 Hosts using the listed VM's and VMHosts
+    # Set the storage policy on EVM02-TNT79 to RAID-1 FTT-1
     Set-AvsVMStoragePolicy -StoragePolicyName "RAID-1 FTT-1" -VMName "EVM02-TNT79"
 #>
 function Set-AvsVMStoragePolicy {
