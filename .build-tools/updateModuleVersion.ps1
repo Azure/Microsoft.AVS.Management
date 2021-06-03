d5eb19d6725cee8f90bc85b310f5156e388c0822
Michael Barta <michaelbarta@microsoft.com>
2021-06-03 15:44:46 -0700

    Removed addition of ExternalDependencies list for unofficial builds.

--- .build-tools/updateModuleVersion.ps1
+++ .build-tools/updateModuleVersion.ps1
@@ -5,6 +5,7 @@ param (
 )
 Write-Output "----START: updateModuleVersion----"
 Write-Output "----IsOfficial: $IsOfficial----"
+
 #Install all RequiredModules in the module manifests because the command Update-ModuleManifest 
 # requires these modules to be on the host in order to run properly.
 $repoRoot = "$env:SYSTEM_DEFAULTWORKINGDIRECTORY"
@@ -15,18 +16,7 @@ Get-Content "$manifestAbsolutePath"
 Write-Output "---- Updating the module version to $env:BUILD_BUILDNUMBER----"
 
 Set-Location "$manifestFolder"
-$targetModuleParams = @{}
-if (!$IsOfficial) {
-    $extModuleArray = @()
-    $requiredModules = (Test-ModuleManifest "$manifestAbsolutePath" -ErrorAction SilentlyContinue).RequiredModules
-    foreach ($module in $requiredModules) {
-        $extModuleArray += $($module.Name)
-    }
-    $targetModuleParams = @{ModuleVersion = "$env:BUILD_BUILDNUMBER"; ExternalModuleDependencies = $extModuleArray ; Path = "$manifestAbsolutePath"}
-    
-}else {
-    $targetModuleParams = @{ModuleVersion = "$env:BUILD_BUILDNUMBER"; Path = "$manifestAbsolutePath"}
-}
+$targetModuleParams = @{ModuleVersion = "$env:BUILD_BUILDNUMBER"; Path = "$manifestAbsolutePath"}
 Update-ModuleManifest @targetModuleParams
 if (!$?) {
     Write-Error -Message "FAILED: Could not update module version"
