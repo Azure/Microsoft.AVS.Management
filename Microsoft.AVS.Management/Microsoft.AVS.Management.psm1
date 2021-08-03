8f9137d5daa5d360144726be04e7e5e2d5f88387
david becher <dab4au@virginia.edu>
2021-08-03 14:56:28 -0700

    Added validation for only VSAN based storage policies and cleaned up output (#44)
    * Added validation for no jetstream sps
    
    * typo
    
    * tooltip changes
    
    * Description changes
    
    * Namespace change for filtering
    
    Co-authored-by: David Becher <dabecher@microsoft.com>

--- Microsoft.AVS.Management/Microsoft.AVS.Management.psm1
+++ Microsoft.AVS.Management/Microsoft.AVS.Management.psm1
@@ -38,7 +38,7 @@ function Get-ProtectedNetworks {
 
 <#
     .Synopsis
-     (NOT RECOMMENDED -> Use New-AvsLDAPSIdentitySource) Allow customers to add an external identity source (Active Directory over LDAP) for use with single sign on to vCenter. Prefaced by Connect-SsoAdminServer
+     Not Recommended (use New-AvsLDAPSIdentitySource): Allow customers to add a not secure external identity source (Active Directory over LDAP) for use with vCenter Single Sign-On.
 
     .Parameter Name
      The user-friendly name the external AD will be given in vCenter
@@ -193,7 +193,7 @@ function New-AvsLDAPIdentitySource {
 
 <#
     .Synopsis
-     Allow customers to add an LDAPS Secure external identity source (Active Directory over LDAP) for use with single sign on to vCenter. Prefaced by Connect-SsoAdminServer
+     Recommended: Allow customers to add a secure external identity source (Active Directory over LDAPS) for use with vCenter Single Sign-On.
 
     .Parameter Name
      The user-friendly name the external AD will be given in vCenter
@@ -447,10 +447,10 @@ function Remove-ExternalIdentitySources {
 
 <#
     .Synopsis
-     Add an external identity group from the external identity to the CloudAdmins group
+     Add a group from the external identity to the CloudAdmins group
 
     .Parameter GroupName
-     Name of the group to be added to CloudAdmins. 
+     Name of the group to be added to the CloudAdmins group. For example, `vsphere-admins`, without the domain appended
 
     .Parameter Domain
      Name of the domain that GroupName is in. If not provided, will attempt to locate the group in all the configured active directories
@@ -586,10 +586,10 @@ function Add-GroupToCloudAdmins {
 
 <#
     .Synopsis
-     Remove a previously added external identity group from the cloud admin group
+     Remove a previously added group from an external identity from the CloudAdmins group
 
     .Parameter GroupName
-     Name of the group to be removed from CloudAdmins. 
+     Short name of the external identity group to be removed from the CloudAdmins group. For example, vsphere-admins, without the domain appended
 
     .Parameter Domain
      Name of the domain that GroupName is in. If not provided, will attempt to locate the group in all the configured active directories
@@ -1019,14 +1019,14 @@ function Set-AvsDrsElevationRule {
 
 <#
     .Synopsis
-     Gets all the storage policies available to set on a VM 
+     Gets all the vSAN based storage policies available to set on a VM.
 #>
 function Get-StoragePolicies {
     [AVSAttribute(3, UpdatesSDDC = $False)]
     
     $StoragePolicies
     try {
-        $StoragePolicies = Get-SpbmStoragePolicy -ErrorAction Stop
+        $StoragePolicies = Get-SpbmStoragePolicy -Namespace "VSAN" -ErrorAction Stop | Select-Object Name, AnyOfRuleSets
     }
     catch {
         Write-Error $PSItem.Exception.Message -ErrorAction Continue
@@ -1043,17 +1043,17 @@ function Get-StoragePolicies {
   
 <#
     .Synopsis
-     Sets the storage policy on the VM to a predefined storage policy
+     Support modifying vSAN based storage policies on individual VMs
 
     .Parameter StoragePolicyName
-     Name of a storage policy to set on the specified VM. Options can be seen in vCenter.
+     Name of a vSAN based storage policy to set on the specified VM. Options can be seen in vCenter or using the Get-StoragePolicies command.
 
     .Parameter VMName
-     Name of the VM to set the storage policy on.
+     Name of the VM to set the vSAN based storage policy on.
 
     .Example 
-    # Set the storage policy on EVM02-TNT79 to RAID-1 FTT-1
-    Set-AvsVMStoragePolicy -StoragePolicyName "RAID-1 FTT-1" -VMName "EVM02-TNT79"
+    # Set the vSAN based storage policy on MyVM to RAID-1 FTT-1
+    Set-AvsVMStoragePolicy -StoragePolicyName "RAID-1 FTT-1" -VMName "MyVM"
 #>
 function Set-AvsVMStoragePolicy {
     [CmdletBinding(PositionalBinding = $false)]
@@ -1075,10 +1075,12 @@ function Set-AvsVMStoragePolicy {
         $VMName
     )
     Write-Host "Getting Storage Policy $StoragePolicyName"
-    $StoragePolicy = Get-SpbmStoragePolicy -Name $StoragePolicyName -ErrorAction Stop
+    $StoragePolicy =  Get-SpbmStoragePolicy -Namespace "VSAN" -ErrorAction Stop | Where-Object {$_.Name -eq $StoragePolicyName}
     if ($null -eq $StoragePolicy) {
-        Write-Error "Could not find Storage Policy with the name $StoragePolicyName" -ErrorAction Stop 
+        Write-Error "Could not find Storage Policy with the name $StoragePolicyName. It either does not exist or is not available." -ErrorAction Continue
+        Write-Error "Available storage policies: $(Get-SpbmStoragePolicy -Namespace "VSAN")" -ErrorAction Stop
     } 
+
     $ProtectedVMs = Get-ProtectedVMs 
     if ($ProtectedVMs.Name.Contains($VMName)) {
         Write-Error "Access denied to this VM." -ErrorAction Stop
