7828ca051b12397dd4b6abb70465c3ab0d740bb0
Michael Barta <michaelbarta@microsoft.com>
2021-08-17 18:15:26 -0700

    [TEST] Prerelease versioning (21).

--- .build-tools/setBuildVersion.ps1
+++ .build-tools/setBuildVersion.ps1
@@ -1,7 +1,7 @@
 #!/usr/bin/pwsh
 param (
     [Parameter(Mandatory=$true)][string]$absolutePathToManifest,
-    [Parameter(Mandatory=$true)][string]$modulePatch
+    [Parameter(Mandatory=$true)][string]$buildPatch
 
 )
 
@@ -9,6 +9,7 @@ $psdFileObj = Import-PowerShellDataFile $absolutePathToManifest
 $manifestVersionAsArray = $psdFileObj.ModuleVersion -split "\."
 $MAJOR = $manifestVersionAsArray[0]
 $minor = $manifestVersionAsArray[1]
+$modulePatch = ($buildPatch -split "\.")[2]
 $updatedBuildVersion = $MAJOR.$minor.$modulePatch
 Write-Host "----- Setting Build Version to $updatedBuildVersion -----"
 Write-Host "##vso[build.updatebuildnumber]$updatedBuildVersion"
