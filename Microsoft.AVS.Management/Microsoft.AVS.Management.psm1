dcf2224e44f266be11cb4522e9f27de1307a22ba
Eugene Tolmachev <eugene.tolmachev@microsoft.com>
2023-03-17 07:30:56 -0400

    revert classes placement

--- Microsoft.AVS.Management/Microsoft.AVS.Management.psm1
+++ Microsoft.AVS.Management/Microsoft.AVS.Management.psm1
@@ -1,6 +1,54 @@
-. $PSScriptRoot\Classes.ps1
 . $PSScriptRoot\UserUtils.ps1
 . $PSScriptRoot\HcxUtils.ps1
+
+<#
+AVSAttribute applied to a commandlet function indicates:
+- whether the SDDC should be marked as Building while the function executes.
+- default timeout for the commandlet, maximum: 3h.
+AVS SDDC in Building state prevents other changes from being made to the SDDC until the function completes/fails.
+#>
+class AVSAttribute : Attribute {
+    [bool]$UpdatesSDDC = $false
+    [TimeSpan]$Timeout
+    AVSAttribute($timeoutMinutes) { $this.Timeout = New-TimeSpan -Minutes $timeoutMinutes }
+}
+
+<#
+SecureFolder class provides a way to create a folder protected from CloudAdmins.
+#>
+class AVSSecureFolder {
+    hidden static [string]$vendors = "AVS-vendor-folders"
+    <#
+        Returns vendors root folder or $null in case of any error.
+    #>
+    static [VMware.VimAutomation.ViCore.Types.V1.Inventory.Folder] Root() {
+        $admin = Get-VIRole -Name "Admin" -ErrorAction Stop
+        $noAccess = Get-VIRole -Name "NoAccess" -ErrorAction Stop
+        $scripting = Get-VIAccount -Id "scripting" -Domain "vsphere.local" -ErrorAction Stop
+        $group = Get-VIAccount -Group -Id "CloudAdmins" -Domain "vsphere.local" -ErrorAction Stop
+        $location = Get-Folder -Location ((Get-Datacenter)[0]) -Name "vm"
+        $root = Get-Folder -Name ([AVSSecureFolder]::vendors) -NoRecursion -Location $location -ErrorAction SilentlyContinue
+        if($null -eq $root) {
+            $root = New-Folder -Location $location -Name ([AVSSecureFolder]::vendors) -ErrorAction Stop
+            New-VIPermission -Entity $root -Principal $scripting -Role $admin -Propagate $true -ErrorAction Stop
+            New-VIPermission -Entity $root -Principal $group -Role $noAccess -Propagate $true -ErrorAction Stop
+        }
+        return $root
+    }
+
+    <#
+        Creates a subfolder or returns existing one given the name.
+        Returns $null in case of any error.
+    #>
+    static [VMware.VimAutomation.ViCore.Types.V1.Inventory.Folder] GetOrCreate([string]$name) {
+        $root = [AVSSecureFolder]::Root()
+        $folder = Get-Folder -Location $root -Name $name -NoRecursion -ErrorAction SilentlyContinue
+        if($null -eq $folder) {
+            $folder = New-Folder -Location $root -Name $name -ErrorAction Stop
+        }
+        return $folder
+    }
+}
 
 <# Download certificate from SAS token url #>
 function Get-Certificates {
