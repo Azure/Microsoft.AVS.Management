aff60def0ce7cad1578b2b074c7808f1f27412ab
rahman-lb <rahman@lightbitslabs.com>
2023-05-09 10:48:41 -0700

    Adding implementation for create/delete/mount/unmount datastore

--- NVMeTCP.AVS.VMFS/NVMeTCP.AVS.VMFS.psm1
+++ NVMeTCP.AVS.VMFS/NVMeTCP.AVS.VMFS.psm1
@@ -1,3 +1,5 @@
+#using module Microsoft.AVS.Management
+
 <# Private Function Import #>
 . $PSScriptRoot\NVMeTCPConstants
 
@@ -68,11 +70,11 @@ function Connect-NVMeTCPTarget {
         $HostEsxcli = Get-EsxCli -VMHost $HostAddress 
     }
     catch {
-        throw " Failed to execute Get-EsxCli cmdlet on host $($HostAddress). Make sure valid ESXi IP/DNS is provided."
+        throw "Failed to execute Get-EsxCli cmdlet on host $($HostAddress). Make sure valid ESXi IP/DNS is provided."
     }
 
     if ($HostEsxcli) { 
-        Write-Host "Connected to host via powercli-esxcli"
+        Write-Host "Connected to host via PowerCLI-esxcli"
      
         try {
    
@@ -95,7 +97,11 @@ function Connect-NVMeTCPTarget {
         Write-Host "Connecting Controller status: "$EsxCliResult;
     }
 
-    Get-VMHostStorage -VMHost $HostAddress -RescanAllHba
+    Write-Host "Rescanning NVMe/TCP storage adapter.."
+
+    $RescanResult = Get-VMHostStorage -VMHost $HostAddress -RescanAllHba 
+    
+    Write-Host "Rescanning Completed."
 
 } 
     
@@ -153,11 +159,12 @@ function Disconnect-NVMeTCPTarget {
     
     $hostEsxcli = $null;
 
-    try {<#Do this if a terminating exception happens#>
+    try {
+        <#Do this if a terminating exception happens#>
 
         $HostEsxcli = Get-EsxCli -VMHost $HostAddress ;
-        Write-Host " EsxCli connected to host $($HostAddress)"
-        if (!$HostEsxcli) {
+        Write-Host "EsxCli connected to host $($HostAddress)"
+        if ($null -eq $HostEsxcli) {
             throw;
         }
     }
@@ -176,7 +183,10 @@ function Disconnect-NVMeTCPTarget {
                 Write-Host "Diconnecting Controller status: "$result;
             }
        
-            Get-VMHostStorage -VMHost $HostAddress -RescanAllHba 
+            Write-Host "Rescanning NVMe/TCP storage adapter.."
+            $RescanResult = Get-VMHostStorage -VMHost $HostAddress -RescanAllHba 
+            
+            Write-Host "Rescanning Completed."
 
         }
        
@@ -190,6 +200,329 @@ function Disconnect-NVMeTCPTarget {
     Write-Host ""
 }
 
+
+<#
+    .SYNOPSIS
+     This function creates VMFS datastore on given ESXi host using NVMe/TCP transport.
+
+     1. vCenter IP address or DNS
+     2. ESXi IP address or DNS
+     3. New Datastore Name
+     4. Device Path on ESXi host
+     
+    .PARAMETER vCenterAddress
+     vCenter IP Address 
+
+    .PARAMETER HostAddress
+     ESXi host IP Address 
+
+    .PARAMETER DatastoreName
+     New Datastore Name
+
+    .PARAMETER DevicePath
+     Device Path on ESXi host
+
+    .EXAMPLE
+     Create-NVMeDatastore -vCenterAddress "192.168.0.1" -HostAddress "192.168.0.10"  -DatastoreName "data-name01" -DevicePath "eui.58204375a0b5408285a89390e1510fec"
+
+    .INPUTS
+     vCenter address, ESXi host address, New datastore name, available NVMe device path 
+
+    .OUTPUTS
+     None.
+#>
+
+function New-NVMeDatastore {
+    Param
+    (
+  
+        [Parameter(
+            Mandatory = $true,
+            HelpMessage = 'vCenter network address')]
+        [string] $vCenterAddress,
+
+        [Parameter(
+            Mandatory = $true,
+            HelpMessage = 'ESXi host network address')]
+        [string] $HostAddress,
+
+        [Parameter(
+            Mandatory = $true,
+            HelpMessage = 'New datastore name')]
+        [string] $DatastoreName,
+
+        [Parameter(
+            Mandatory = $true,
+            HelpMessage = 'NVMe device path')]
+        [string]     $DevicePath
+
+    )
+
+    Write-Host "Creating new datastore $($DatastoreName) on vCenter Server "  $vCenterAddress 
+     
+    $AvailableDatastore = $null
+     
+    try {
+        Write-Host "Checking if datastore already exist.."
+        $AvailableDatastore = Get-Datastore -Name $DatastoreName  -ErrorAction ignore
+        if ($AvailableDatastore) {
+            Write-Host "Datastore with given name alreay exist, use different name for new datastore"
+            Exit 
+
+        } 
+
+    }
+    catch {
+        Write-Host "Get-Datastore didn't find datastore by name $($DatastoreName)."
+    }
+
+    try {
+        
+        Write-Host "Creating datastore now.. "
+        $result = New-Datastore -Vmfs -FileSystemVersion 6 -VMHost $HostAddress -Name $DatastoreName -Path $DevicePath
+        Write-Host $result
+          
+        Write-Host "Rescanning NVMe/TCP storage adapter.."
+
+        $RescanResult = Get-VMHostStorage -VMHost $HostAddress -RescanAllHba 
+    
+        Write-Host "Rescanning Completed."
+
+        $AvailableDatastore = Get-Datastore -Name $DatastoreName  -ErrorAction ignore
+
+        if ($AvailableDatastore) {
+            Write-Host "New datastore created successfully"
+        }
+
+    }
+    catch {
+        throw "Failed to create  new datastore on host $($HostAddress)."
+    }
+  
+    Write-Host " " ;
+     
+}
+
+<#
+    .SYNOPSIS
+     This function removes VMFS datastore on a given ESXi host using NVMe/TCP transport.
+
+     1. ESXi IP address or DNS
+     2. Datastore Name
+     
+    .PARAMETER HostAddress
+     ESXi host IP Address 
+
+    .PARAMETER DatastoreName
+     Datastore Name
+
+    
+    .EXAMPLE
+     Remove-NVMeDatastore -HostAddress "192.168.0.10"  -DatastoreName "data-name01" 
+
+    .INPUTS
+     ESXi host address, Datastore name 
+
+    .OUTPUTS
+     None.
+#>
+
+function Remove-NVMeDatastore {
+    Param
+    (
+  
+        [Parameter(
+            Mandatory = $true,
+            HelpMessage = 'ESXi host network address')]
+        [string] $HostAddress,
+
+        [Parameter(
+            Mandatory = $true,
+            HelpMessage = 'New datastore name')]
+        [string] $DatastoreName
+    )
+       
+    Write-Host "Removing datastore $($DatastoreName) on ESXi host"  $HostAddress
+     
+    $AvailableDatastore = $null
+     
+    try {
+        Write-Host "Checking if datastore already exist.."
+        $AvailableDatastore = Get-Datastore -Name $DatastoreName -ErrorAction ignore
+        if ($AvailableDatastore) {
+            Write-Host "Removing the Datastore.. "
+            try {
+                Remove-Datastore -VMHost $HostAddress $DatastoreName -Confirm:$false    
+                Write-Host "Datastores removed. "
+                Write-Host "Rescanning datastore "
+                Get-VMHostStorage -VMHost $HostAddress -RescanAllHba -RescanVmfs
+            }
+            catch {
+                Write-Host "Failed to delete datasore $($DatastoreName)."
+            }
+               
+        } 
+        else {
+            Write-Host "Didn't find datasore $($DatastoreName) to delete."
+        } 
+
+    }
+    catch {
+        Write-Host "Get-Datastore didn't find datastore $($DatastoreName)."
+    }
+    Write-Host " " ;
+     
+}
+
+
+<#
+    .SYNOPSIS
+     This function mount VMFS datastore to already attached ESXi host(s) using NVMe/TCP transport.
+
+     1. Datastore Name
+     
+    .PARAMETER DatastoreName
+     Datastore Name
+
+    
+    .EXAMPLE
+     Mount-NVMeDatastore -DatastoreName "datastore-name" 
+
+    .INPUTS
+     Datastore name 
+
+    .OUTPUTS
+     None.
+#>
+
+Function Mount-NVMeDatastore {
+    Param(
+        [Parameter(
+            Mandatory = $true,
+            HelpMessage = 'Datastore name')]
+        [string] $DatastoreName
+    )
+
+    Process {
+        if (-not $DatastoreName) {
+            Write-Host "No Datastore name provided"
+            Exit
+        }
+
+        $Datastore = $null 
+        try {
+            $Datastore = Get-Datastore -Name $DatastoreName -ErrorAction ignore
+        }
+        catch {
+            throw " Failed to execute Get-Datastore by name  $($DatastoreName)."
+        }
+		
+        if ( $null -eq $Datastore) {
+            throw " No datastore found by the given name $($DatastoreName) to mount "
+        } 
+        
+
+        if ($Datastore) {
+            Write-Host "Datastore found and mounting on all associated ESXi host(s)."
+            $hostviewDSDiskName = $Datastore.ExtensionData.Info.vmfs.extent[0].Diskname
+            
+
+            if ($Datastore.ExtensionData.Host) {
+                $attachedHosts = $Datastore.ExtensionData.Host
+                Foreach ($VMHost in $attachedHosts) {
+                    if (!($VMHost.MountInfo.Accessible)) {
+                        $hostview = Get-View $VMHost.Key
+                        $StorageSys = Get-View $HostView.ConfigManager.StorageSystem
+                        Write-Host "Mounting VMFS Datastore $($Datastore.Name) on host $($hostview.Name)..."
+                        $StorageSys.MountVmfsVolume($Datastore.ExtensionData.Info.vmfs.uuid);
+                        Write-Host "Datastore $($Datastore.Name) mounted successfully on host $($hostview.Name)."
+                    }
+                    else {
+                        Write-Host "No action needed, datastore is accessible to host " $VMHost.Key
+                    } 
+
+                }
+            }
+        }
+
+        Write-Host " "
+     
+    }
+}
+
+
+
+<#
+    .SYNOPSIS
+     This function unmount VMFS datastore from ESXi host(s) using NVMe/TCP transport.
+
+     1. Datastore Name
+     
+    .PARAMETER DatastoreName
+     Datastore Name
+    
+    .EXAMPLE
+     Unmount-NVMeDatastore -DatastoreName "datastore-name" 
+
+    .INPUTS
+     Datastore name 
+
+    .OUTPUTS
+     None.
+#>
+
+Function Unmount-NVMeDatastore {
+    [CmdletBinding()]
+   
+    Param(
+        [Parameter(
+            Mandatory = $true,
+            HelpMessage = 'Datastore name')]
+        [string] $DatastoreName
+    )
+
+    Process {
+        if (-not $DatastoreName) {
+            Write-Host "No Datastore name provided"
+            Exit
+        }
+
+        $Datastore = $null 
+        try {
+            $Datastore = Get-Datastore -Name $DatastoreName -ErrorAction ignore
+        }
+        catch {
+            throw " No datastore found by the given name $($DatastoreName)."
+        }
+		
+        if ($Datastore) {
+            Write-Host "Datastore found and unmounting from ESXi host(s)."
+            $hostviewDSDiskName = $Datastore.ExtensionData.Info.vmfs.extent[0].Diskname
+            
+            if ($Datastore.ExtensionData.Host) {
+                $attachedHosts = $Datastore.ExtensionData.Host
+                Foreach ($VMHost in $attachedHosts) {
+                    if ($VMHost.MountInfo.Accessible) {
+                        $hostview = Get-View $VMHost.Key
+                        $StorageSys = Get-View $HostView.ConfigManager.StorageSystem
+                        Write-Host "Unmounting VMFS Datastore $($Datastore.Name) from host $($hostview.Name).."
+                        $StorageSys.UnmountVmfsVolume($Datastore.ExtensionData.Info.vmfs.uuid);
+                        Write-Host "Datastore $($Datastore.Name) unmounted successfully from host $($hostview.Name) ."
+                    }
+                    else {
+                        Write-Host "No action needed, datastore is not accessible to host " $VMHost.Key
+                    } 
+
+                }
+            }
+        }
+
+        Write-Host " "
+
+    }
+
+  
+}
 
 
 
