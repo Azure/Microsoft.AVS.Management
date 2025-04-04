b2873317657d4c639906e2fda0bc74af14c08f68
Eugene Tolmachev <eugene.tolmachev@microsoft.com>
2025-04-04 16:23:17 -0400

    use consumption feed

--- .build-tools/getRequiredModules.ps1
+++ .build-tools/getRequiredModules.ps1
@@ -1,17 +1,16 @@
 #!/usr/bin/pwsh
 param (
+    [Parameter(Mandatory=$true)][string]$accessToken,
     [Parameter(Mandatory=$true)][string]$psdPath
 )
+$c =  [PSCredential]::new("ado", ($accessToken | ConvertTo-SecureString -AsPlainText -Force))
 $requiredModules = (Test-ModuleManifest "$psdPath" -ErrorAction SilentlyContinue).RequiredModules
-Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
-
 foreach ($module in $requiredModules) {
     $targetModule = $($module.Name)
     $targetVersion = $($module.Version)
     Write-Host "Installing $targetModule-$targetVersion ...."
-    Install-Module $targetModule -RequiredVersion $targetVersion -Repository PSGallery
-    Write-Host "----COMPLETED installation of $targetModule-$targetVersion----"
+    Install-Module $targetModule -RequiredVersion $targetVersion -Repository Consumption -Credential $c
 }
 
-
-Write-Host "----SUCCESS: installed all required modules----"
+Install-Module PSScriptAnalyzer -RequiredVersion 1.21.0 -Repository Consumption -Credential $c
+Install-Module Pester -SkipPublisherCheck -MinimumVersion 5.0 -Repository Consumption -Credential $c
