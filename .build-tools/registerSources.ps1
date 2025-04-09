7613a2eb7da0482ecbd3f4be4798878eba9125ae
Eugene Tolmachev <eugene.tolmachev@microsoft.com>
2025-04-09 11:30:33 -0400

    use v3 publishing

--- .build-tools/registerSources.ps1
+++ .build-tools/registerSources.ps1
@@ -13,7 +13,13 @@ Register-PSResourceRepository -Name ConsumptionV3 -Trusted -Uri "$consumptionFee
 if( [Uri]::TryCreate($previewFeed, [UriKind]::Absolute, [ref]$uri) -eq $false) {
     throw "Invalid feed URI: $previewFeed"
 }
-if($uri.IsFile) { mkdir -p $previewFeed }
+if($uri.IsFile) {
+    mkdir -p $previewFeed
+    Register-PackageSource -Name Preview -ProviderName PowerShellGet -Location $previewFeed -PublishLocation $previewFeed
+    Register-PSResourceRepository -Name PreviewV3 -Trusted -Uri $previewFeed
+}
+else {
+    Register-PackageSource -Name Preview -ProviderName PowerShellGet -Location "$previewFeed/nuget/v2" -PublishLocation "$previewFeed/nuget/v2"
+    Register-PSResourceRepository -Name PreviewV3 -Trusted -Uri "$previewFeed/nuget/v3/index.json"
+}
 
-Register-PackageSource -Name Preview -ProviderName PowerShellGet -Location $previewFeed -PublishLocation $previewFeed
-Register-PSResourceRepository -Name PreviewV3 -Trusted -Uri $previewFeed.Replace("/nuget/v2","/nuget/v3/index.json")
