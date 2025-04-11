3cb819748f5c977d4daec850eeb1ffea2effb21e
Eugene Tolmachev <eugene.tolmachev@microsoft.com>
2025-04-11 16:00:13 -0400

    install fixed psresourceget

--- .build-tools/getRequiredModules.ps1
+++ .build-tools/getRequiredModules.ps1
@@ -4,6 +4,9 @@ param (
     [Parameter(Mandatory=$true)][string]$psdPath
 )
 $c =  [PSCredential]::new("ado", ($accessToken | ConvertTo-SecureString -AsPlainText -Force))
+# https://github.com/PowerShell/PSResourceGet/issues/1777
+Install-Module Microsoft.PowerShell.PSResourceGet -requiredversion 1.1.1 -Repository Consumption -Credential $c -Force -SkipPublisherCheck
+
 $requiredModules = (Test-ModuleManifest "$psdPath" -ErrorAction SilentlyContinue).RequiredModules
 foreach ($module in $requiredModules) {
     $targetModule = $($module.Name)
