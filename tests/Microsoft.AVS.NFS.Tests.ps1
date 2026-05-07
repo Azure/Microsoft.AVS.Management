104258328466783f77c9e5b367571b6f02a7da5a
mchhibber <mchhibber@microsoft.com>
2026-05-08 02:39:20 +0530

    resolved all comments

--- tests/Microsoft.AVS.NFS.Tests.ps1
+++ tests/Microsoft.AVS.NFS.Tests.ps1
@@ -36,6 +36,12 @@ BeforeAll {
         process { $null }
     }
 
+    # Override Get-EsxCli with a permissive stub so PSCustomObject mock hosts pass parameter binding
+    function global:Get-EsxCli {
+        param($VMHost, [switch]$V2, $Server)
+        $null
+    }
+
     # Stub for Limit-WildcardsandCodeInjectionCharacters from Microsoft.AVS.Management
     if (-not (Get-Command 'Limit-WildcardsandCodeInjectionCharacters' -ErrorAction SilentlyContinue)) {
         function global:Limit-WildcardsandCodeInjectionCharacters {
@@ -183,4 +189,152 @@ Describe "Get-NFSDatastoreNConnectValue" {
         }
     }
 
+    Context "Happy Path - NConnect Value Retrieval" {
+        BeforeAll {
+            $mockCluster = [PSCustomObject]@{ Name = "TestCluster" }
+            Mock Get-Cluster { $mockCluster } -ModuleName Microsoft.AVS.NFS
+            Mock Get-Datastore { [PSCustomObject]@{ Name = "NfsDS"; Type = "NFS" } } -ModuleName Microsoft.AVS.NFS
+
+            # Two connected hosts and one disconnected host (which should be skipped)
+            Mock Get-VMHost {
+                @(
+                    [PSCustomObject]@{ Name = "esxi-01"; ConnectionState = "Connected" },
+                    [PSCustomObject]@{ Name = "esxi-02"; ConnectionState = "Connected" },
+                    [PSCustomObject]@{ Name = "esxi-03"; ConnectionState = "Disconnected" }
+                )
+            } -ModuleName Microsoft.AVS.NFS
+
+            # Mock Get-EsxCli to return an object with a storage.nfs.list.invoke() chain
+            Mock Get-EsxCli {
+                $listObj = New-Object PSObject
+                $listObj | Add-Member -MemberType ScriptMethod -Name invoke -Value {
+                    @(
+                        [PSCustomObject]@{
+                            VolumeName  = "NfsDS"
+                            Host        = "10.0.0.10"
+                            Share       = "/exports/nfsds"
+                            NFSv41      = $false
+                            Connections = 4
+                            Accessible  = $true
+                            Mounted     = $true
+                        }
+                    )
+                }
+                [PSCustomObject]@{
+                    storage = [PSCustomObject]@{
+                        nfs = [PSCustomObject]@{
+                            list = $listObj
+                        }
+                    }
+                }
+            } -ModuleName Microsoft.AVS.NFS
+        }
+
+        AfterAll {
+            Remove-Variable -Name NamedOutputs -Scope Global -ErrorAction SilentlyContinue
+        }
+
+        It "Should populate NamedOutputs for connected hosts and skip disconnected hosts" {
+            Remove-Variable -Name NamedOutputs -Scope Global -ErrorAction SilentlyContinue
+
+            Get-NFSDatastoreNConnectValue -ClusterName "TestCluster" -DatastoreName "NfsDS"
+
+            $global:NamedOutputs | Should -Not -BeNullOrEmpty
+            $global:NamedOutputs.Count | Should -Be 2
+            $global:NamedOutputs.Keys | Should -Contain "esxi-01"
+            $global:NamedOutputs.Keys | Should -Contain "esxi-02"
+            $global:NamedOutputs.Keys | Should -Not -Contain "esxi-03"
+
+            $global:NamedOutputs["esxi-01"] | Should -Match "NConnectValue : 4"
+            $global:NamedOutputs["esxi-01"] | Should -Match "DatastoreName : NfsDS"
+            $global:NamedOutputs["esxi-01"] | Should -Match "NfsServerHost : 10.0.0.10"
+            $global:NamedOutputs["esxi-01"] | Should -Match "SharePath : /exports/nfsds"
+            $global:NamedOutputs["esxi-01"] | Should -Match "NfsVersion : 3"
+        }
+
+        It "Should call Get-EsxCli only for connected hosts" {
+            Get-NFSDatastoreNConnectValue -ClusterName "TestCluster" -DatastoreName "NfsDS" 
+            Should -Invoke Get-EsxCli -ModuleName Microsoft.AVS.NFS -Times 2 -Exactly
+        }
+    }
+
+    Context "AVSAttribute Verification" {
+        It "Should have AVSAttribute with 10 minute timeout" {
+            $command = Get-Command Get-NFSDatastoreNConnectValue
+            $avsAttr = $command.ScriptBlock.Attributes | Where-Object { $_.TypeId.Name -eq 'AVSAttribute' }
+            $avsAttr | Should -Not -BeNullOrEmpty
+            $avsAttr.Timeout.TotalMinutes | Should -Be 10
+        }
+
+        It "Should have AVSAttribute with UpdatesSDDC set to false" {
+            $command = Get-Command Get-NFSDatastoreNConnectValue
+            $avsAttr = $command.ScriptBlock.Attributes | Where-Object { $_.TypeId.Name -eq 'AVSAttribute' }
+            $avsAttr.UpdatesSDDC | Should -Be $false
+        }
+
+        It "Should have AVSAttribute timeout <= 60 minutes" {
+            $command = Get-Command Get-NFSDatastoreNConnectValue
+            $avsAttr = $command.ScriptBlock.Attributes | Where-Object { $_.TypeId.Name -eq 'AVSAttribute' }
+            $avsAttr.Timeout.TotalMinutes | Should -BeLessOrEqual 60
+        }
+    }
+
+}
+
+Describe "New-NFSDatastore" {
+    Context "AVSAttribute Verification" {
+        It "Should have AVSAttribute with 10 minute timeout" {
+            $command = Get-Command New-NFSDatastore
+            $avsAttr = $command.ScriptBlock.Attributes | Where-Object { $_.TypeId.Name -eq 'AVSAttribute' }
+            $avsAttr | Should -Not -BeNullOrEmpty
+            $avsAttr.Timeout.TotalMinutes | Should -Be 10
+        }
+
+        It "Should have AVSAttribute with UpdatesSDDC set to false" {
+            $command = Get-Command New-NFSDatastore
+            $avsAttr = $command.ScriptBlock.Attributes | Where-Object { $_.TypeId.Name -eq 'AVSAttribute' }
+            $avsAttr.UpdatesSDDC | Should -Be $false
+        }
+
+        It "Should have AVSAttribute with AutomationOnly set to true" {
+            $command = Get-Command New-NFSDatastore
+            $avsAttr = $command.ScriptBlock.Attributes | Where-Object { $_.TypeId.Name -eq 'AVSAttribute' }
+            $avsAttr.AutomationOnly | Should -Be $true
+        }
+
+        It "Should have AVSAttribute timeout <= 60 minutes" {
+            $command = Get-Command New-NFSDatastore
+            $avsAttr = $command.ScriptBlock.Attributes | Where-Object { $_.TypeId.Name -eq 'AVSAttribute' }
+            $avsAttr.Timeout.TotalMinutes | Should -BeLessOrEqual 60
+        }
+    }
+}
+
+Describe "Remove-NFSDatastore" {
+    Context "AVSAttribute Verification" {
+        It "Should have AVSAttribute with 10 minute timeout" {
+            $command = Get-Command Remove-NFSDatastore
+            $avsAttr = $command.ScriptBlock.Attributes | Where-Object { $_.TypeId.Name -eq 'AVSAttribute' }
+            $avsAttr | Should -Not -BeNullOrEmpty
+            $avsAttr.Timeout.TotalMinutes | Should -Be 10
+        }
+
+        It "Should have AVSAttribute with UpdatesSDDC set to false" {
+            $command = Get-Command Remove-NFSDatastore
+            $avsAttr = $command.ScriptBlock.Attributes | Where-Object { $_.TypeId.Name -eq 'AVSAttribute' }
+            $avsAttr.UpdatesSDDC | Should -Be $false
+        }
+
+        It "Should have AVSAttribute with AutomationOnly set to true" {
+            $command = Get-Command Remove-NFSDatastore
+            $avsAttr = $command.ScriptBlock.Attributes | Where-Object { $_.TypeId.Name -eq 'AVSAttribute' }
+            $avsAttr.AutomationOnly | Should -Be $true
+        }
+
+        It "Should have AVSAttribute timeout <= 60 minutes" {
+            $command = Get-Command Remove-NFSDatastore
+            $avsAttr = $command.ScriptBlock.Attributes | Where-Object { $_.TypeId.Name -eq 'AVSAttribute' }
+            $avsAttr.Timeout.TotalMinutes | Should -BeLessOrEqual 60
+        }
+    }
 }
